3696193ba9e866830f96653959bea439017c0810
David Blevins <david.blevins@gmail.com>
2019-05-30 12:44:12 -0700

    Very basic Crest Starter project

---
+++ src/test/java/org/eclipse/wg/jakartaee/AppTest.java
@@ -0,0 +1,21 @@
+package org.eclipse.wg.jakartaee;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.tomitribe.crest.Main;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends Assert {
+
+    @Test
+    public void testApp() throws Exception {
+        final Main main = new Main(App.class);
+
+        assertEquals("Hello, World!", main.exec("hello"));
+        assertEquals("Hello, Wisconsin!", main.exec("hello", "--name=Wisconsin"));
+        assertEquals("Hola, Ecuador!", main.exec("hello", "--name=Ecuador", "--language=ES"));
+    }
+}
