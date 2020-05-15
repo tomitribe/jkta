611c957cd08702a2cf2fcfc4873c11f39c87356f
David Blevins <david.blevins@gmail.com>
2020-05-15 15:07:15 -0700

    Expanded bytecode tests

---
+++ src/test/java/org/tomitribe/jkta/Asmifier.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomitribe.jkta;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.util.ASMifier;
+import org.objectweb.asm.util.TraceClassVisitor;
+import org.tomitribe.util.Hex;
+import org.tomitribe.util.IO;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class Asmifier {
+
+    private static final AtomicReference<File> tmpDir = new AtomicReference<File>(null);
+
+    static {
+        try {
+            final File asmifier = File.createTempFile("Asmifier", null);
+            tmpDir.set(asmifier.getParentFile());
+            if (!asmifier.delete()) {
+                asmifier.deleteOnExit();
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException("Asmifier failed to locate a temporary directory");
+        }
+    }
+
+    public static void hex(final ClassLoader classLoader, final String className) throws IOException {
+        final String internalName = className.replace('.', '/') + ".class";
+        final URL resource = classLoader.getResource(internalName);
+
+        if (null == resource) {
+            throw new IOException("Failed to find resource: " + internalName);
+        }
+        System.out.println(className);
+        final InputStream inputStream = resource.openStream();
+        byte[] bytes = IO.readBytes(inputStream);
+        for (final byte b : bytes) {
+            System.out.printf("%-2s %-2s%n", Hex.toString(new byte[]{b}), (char) b);
+        }
+    }
+
+    public static void print(final ClassLoader classLoader, final String className) throws IOException {
+        final String internalName = className.replace('.', '/') + ".class";
+        final URL resource = classLoader.getResource(internalName);
+
+        if (null == resource) {
+            throw new IOException("Failed to find resource: " + internalName);
+        }
+
+        final org.objectweb.asm.ClassReader reader = new org.objectweb.asm.ClassReader(resource.openStream());
+
+
+        final File file = new File("/tmp/" + className);
+
+        write(reader, file);
+    }
+
+    private static void write(final ClassReader reader, final File file) throws IOException {
+        final OutputStream write = IO.write(file);
+        write(reader, write);
+    }
+
+    public static void write(final ClassReader reader, final OutputStream write) throws IOException {
+        final TraceClassVisitor visitor = new TraceClassVisitor(null, new ASMifier(), new PrintWriter(write));
+        reader.accept(visitor, ClassReader.SKIP_DEBUG);
+        write.close();
+    }
+
+    public static void asmify(final Class clazz, final String suffix) throws IOException {
+        asmify(clazz.getName(), Bytecode.readClassFile(clazz.getClassLoader(), clazz), suffix);
+    }
+
+    public static void asmify(final ClassLoader loader, final String className, final String suffix) throws IOException {
+        asmify(className, Bytecode.readClassFile(loader, className), suffix);
+    }
+
+    public static void asmify(final String className, final byte[] bytes, final String suffix) throws IOException {
+        final org.objectweb.asm.ClassReader reader = new org.objectweb.asm.ClassReader(bytes);
+        final File file = new File(tmpDir.get(), className + "." + suffix);
+
+        write(reader, file);
+    }
+
+    public static String asmify(final byte[] actualBytes) throws IOException {
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        write(new ClassReader(actualBytes), byteArrayOutputStream);
+        return new String(byteArrayOutputStream.toByteArray());
+    }
+}
--- src/test/java/org/tomitribe/jkta/usage/BytecodeUsageTest.java
+++ src/test/java/org/tomitribe/jkta/usage/BytecodeUsageTest.java
@@ -19,16 +19,29 @@ package org.tomitribe.jkta.usage;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.tomitribe.jkta.Asmifier;
+import org.tomitribe.jkta.Bytecode;
 import org.tomitribe.util.Archive;
 
 import javax.ejb.EJBException;
 import javax.ejb.LockType;
 import javax.ejb.SessionBean;
+import javax.ejb.Singleton;
+import javax.persistence.Id;
 import javax.transaction.Transaction;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class BytecodeUsageTest extends Assert {
 
@@ -55,6 +68,75 @@ public class BytecodeUsageTest extends Assert {
 
         assertEquals(1, usage.getJavax());
         assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Ignore
+    @Test
+    public void testField() throws Exception {
+
+        final Usage<Jar> usage = usage(new Object() {
+            private SessionBean sb = null;
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testFieldGenericDeclaration() throws Exception {
+
+        final Usage<Jar> usage = usage(new Object() {
+            private AtomicReference<SessionBean> sb = null;
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    /**
+     * These are compiled away
+     */
+    @Test
+    public void testVariableGenericAssignment() throws Exception {
+
+        final Usage<Jar> usage = usage(new Object() {
+            public void foo() {
+                AtomicReference<?> sb = new AtomicReference<SessionBean>();
+            }
+        });
+
+        assertEquals(0, usage.getJavax());
+        assertEquals(0, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Ignore
+    @Test
+    public void testVariableDeclaration() throws Exception {
+
+        final Usage<Jar> usage = usage(new Object() {
+            public void foo() {
+                SessionBean sb = null;
+                if (sb == null) {
+                    System.out.println(sb);
+                }
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testVariableGenericDeclaration() throws Exception {
+
+        final Usage<Jar> usage = usage(new Object() {
+            public void foo() {
+                AtomicReference<?> sb = new AtomicReference<SessionBean>();
+            }
+        });
+
+        assertEquals(0, usage.getJavax());
+        assertEquals(0, usage.get(Package.JAVAX_EJB));
     }
 
     @Test
@@ -117,11 +199,129 @@ public class BytecodeUsageTest extends Assert {
         assertEquals(2, usage.get(Package.JAVAX_EJB));
     }
 
+    @Test
+    public void testInstanceof() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                boolean b = o instanceof SessionBean;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testIfInstanceof() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                if (o instanceof SessionBean) {
+                    System.out.println();
+                }
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testTernaryInstanceof() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                final Object o2 = o instanceof SessionBean ? "" : null;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testWhileInstanceof() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(Object o) {
+                while (o instanceof SessionBean) {
+                    o = System.currentTimeMillis();
+                }
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testHardEquals() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                boolean b = o == SessionBean.class;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testIfHardEquals() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                if (o == SessionBean.class) {
+                    System.out.println();
+                }
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testTernaryHardEquals() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(final Object o) {
+                final Object o2 = o == SessionBean.class ? "" : null;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testWhileHardEquals() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public void get(Object o) {
+                while (o == SessionBean.class) {
+                    o = System.currentTimeMillis();
+                }
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
     @Ignore
     @Test
     public void testGenericMethodParam() throws Exception {
         final Usage<Jar> usage = usage(new Object() {
-            public void get(final Reference<LockType> c) {
+            public void get(final Reference<SessionBean> c) {
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+
+    @Test
+    public void testGenericField() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            final Reference<SessionBean> c = new WeakReference<>(null);
+
+            public void get() {
             }
         });
 
@@ -141,8 +341,84 @@ public class BytecodeUsageTest extends Assert {
         assertEquals(1, usage.get(Package.JAVAX_EJB));
     }
 
+
+    @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+    @Retention(RetentionPolicy.RUNTIME)
+    public static @interface Clazz {
+        Class<?> value();
+    }
+
+    @Test
+    public void testAnnotationParameter() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            @Clazz(SessionBean.class)
+            public void get() {
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Test
+    public void testFieldAnnotation() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            @Id
+            private long foo;
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_PERSISTENCE));
+    }
+
+    @Test
+    public void testMethodAnnotation() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            @GET
+            public String get() {
+                return null;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_WS_RS));
+    }
+
+    @Ignore
+    @Test
+    public void testParameterAnnotation() throws Exception {
+        final Usage<Jar> usage = usage(new Object() {
+            public String get(@PathParam("foo") String foo) {
+                return null;
+            }
+        });
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_WS_RS));
+    }
+
+    @Ignore
+    @Test
+    public void testClassAnnotation() throws Exception {
+        final Usage<Jar> usage = usage(AnnotatedClass.class);
+
+        assertEquals(1, usage.getJavax());
+        assertEquals(1, usage.get(Package.JAVAX_EJB));
+    }
+
+    @Singleton
+    public static class AnnotatedClass {
+        
+    }
+
     private static Usage<Jar> usage(final Object o) throws IOException, NoSuchAlgorithmException {
-        final Class<? extends Object> aClass = o.getClass();
+        return usage(o.getClass());
+    }
+
+    private static Usage<Jar> usage(final Class<?> aClass) throws IOException, NoSuchAlgorithmException {
+        final ClassLoader loader = aClass.getClassLoader();
+        System.out.println(Asmifier.asmify(Bytecode.readClassFile(loader, aClass)));
+        System.out.println();
         final File jar = Archive.archive().add(aClass).toJar();
         return JarUsage.of(jar);
     }
